1bc88a100a30d58e96462f819fcb1ad7fde041b4
andresagraf <andres.agraf@gmail.com>
2016-01-04 18:41:35 -0300

    PROGRAMA

--- contenido/themes/summit_2016/page_programa.php
+++ contenido/themes/summit_2016/page_programa.php
@@ -5,9 +5,50 @@
 
 get_header(); ?>
 
+<section>
+<div class="cabecera_pagina_interior">
+        <div class="texto_slider2">
+        <div class="container ">
+    	
+        <div class="row">
+        	<div class="col-md-12"  >
+           		 <h1>PROGRAMA SUMMIT 2016</h1>
+            	<p>dasdasñ lkañsld kasñl dksañl dkñsl kdsañlk dañl kdñk dasdasñ lkañsld kasñl dksañl dkñsl kdsañlk dañl kdñk dasdasñ lkañsld kasñl dksañl dkñsl kdsañlk dañl kdñk dasdasñ lkañsld kasñl dksañl dkñsl kdsañlk dañl kdñk</p>
+        	</div>
+        </div>
+        </div>
+    </div>
+    <div class="fx_slider2">
+    	<img src="<?php echo get_template_directory_uri(); ?>/images/fx_header.png"  alt=""/>
+    </div>
+	<img class="imagen_pagina" src="<?php echo get_template_directory_uri(); ?>/images/shutterstock_120995251.jpg"  alt=""/> 
 
-programas
+</div>
 
+</section>
+<section>
+<div class="container">
+	<div class="row">
+    	<div class="col-md-4">
+        	<div class="caja-dia">MARTES 8</div>
+            <div class="caja-charlas-dia"></div>
+         </div>
+        <div class="col-md-4">
+        	<div class="caja-dia">MIÉRCOLES 9</div>        
+            <div class="caja-charlas-dia"></div>            
+        </div>
+        <div class="col-md-4">
+        	<div class="caja-dia">JUEVES 10</div>        
+            <div class="caja-charlas-dia"></div>            
+        </div>
+    </div>
+
+</div>
+
+</section>
+<section>
+<h3>MESA REDONDA</h3>
+</section>
 
 <?php
 get_footer();
